49ce0d55469b2dc4c8e6797a420aba567f83353f
Ross W. Montgomery <rwmontgomery@ross-imac.local>
2017-07-04 04:58:58 -0400

    Initial Commit

---
+++ PokeDex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by Ross W. Montgomery on 7/4/17.
+//  Copyright © 2017 Montgomery Vision. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
